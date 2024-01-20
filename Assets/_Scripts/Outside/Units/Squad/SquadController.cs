0a535e288d4c7735c93559201ff95639d32ee640
Darrel Traballo <barreltraballo1121@gmail.com>
2024-01-21 00:39:25 +0800

    completely separated mouse controls
    left mouse -> selecting and moving units
    right mouse -> moving camera around

--- Assets/_Scripts/Outside/Units/Squad/SquadController.cs
+++ Assets/_Scripts/Outside/Units/Squad/SquadController.cs
@@ -6,11 +6,13 @@ namespace ReplayValue
 {
     public class SquadController : MonoBehaviour
     {
+        [SerializeField] private float circleRadius; // ignore this circleRadius variable in this code
         [SerializeField] private Transform selectionAreaTransform;
-        [SerializeField] private float circleRadius;
 
         private Vector3 startPos;
         private List<SquadUnit> selectedSquadUnits;
+
+        private bool isDragging = false;
 
         private void Awake()
         {
@@ -22,65 +24,92 @@ namespace ReplayValue
         {
             if (Input.GetMouseButtonDown(0))
             {
-                selectionAreaTransform.gameObject.SetActive(true);
                 startPos = UtilsClass.GetMouseWorldPosition();
+
+                if (!isDragging)
+                {
+                    HandleUnitMovement();
+                }
             }
 
             if (Input.GetMouseButton(0))
             {
-                Vector3 currentMousePos = UtilsClass.GetMouseWorldPosition();
-
-                Vector3 lowerLeft = new Vector3(
-                    Mathf.Min(startPos.x, currentMousePos.x),
-                    Mathf.Min(startPos.y, currentMousePos.y)
-                );
-
-                Vector3 upperRight = new Vector3(
-                    Mathf.Max(startPos.x, currentMousePos.x),
-                    Mathf.Max(startPos.y, currentMousePos.y)
-                );
-
-                selectionAreaTransform.position = lowerLeft;
-
-                selectionAreaTransform.localScale = upperRight - lowerLeft;
+                isDragging = true;
+                HandleSelectionArea();
             }
 
             if (Input.GetMouseButtonUp(0))
             {
-                Collider2D[] foundColliders = Physics2D.OverlapAreaAll(startPos, UtilsClass.GetMouseWorldPosition());
-                selectionAreaTransform.gameObject.SetActive(false);
-
-                foreach (var unit in selectedSquadUnits)
+                if (isDragging)
                 {
-                    unit.SetSelectedVisible(false);
+                    SelectMultipleUnits();
                 }
 
-                selectedSquadUnits.Clear();
-
-                foreach (var foundCollider in foundColliders)
-                {
-                    if (foundCollider.TryGetComponent(out SquadUnit unit))
-                    {
-                        selectedSquadUnits.Add(unit);
-                        unit.SetSelectedVisible(true);
-                    }
-                }
-
+                isDragging = false;
                 circleRadius = Mathf.Clamp(selectedSquadUnits.Count * 2 + 1, 5f, 15f);
             }
 
-            if (Input.GetMouseButtonDown(1))
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
-                Vector3 targetPosition = UtilsClass.GetMouseWorldPosition();
+                DeselectUnits();
+            }
+        }
 
+        private void SelectMultipleUnits()
+        {
+            Collider2D[] foundColliders = Physics2D.OverlapAreaAll(startPos, UtilsClass.GetMouseWorldPosition());
+            selectionAreaTransform.gameObject.SetActive(false);
 
-                foreach (var unit in selectedSquadUnits)
+            foreach (var foundCollider in foundColliders)
+            {
+                if (foundCollider.TryGetComponent(out SquadUnit unit))
                 {
-                    Vector3 randomPos = targetPosition + new Vector3(Random.insideUnitCircle.x, Random.insideUnitCircle.y, 0f) * circleRadius;
-
-                    unit.SetTargetPosition(randomPos);
+                    selectedSquadUnits.Add(unit);
+                    unit.SetSelectedVisible(true);
                 }
             }
+        }
+
+        private void HandleSelectionArea()
+        {
+            selectionAreaTransform.gameObject.SetActive(true);
+            Vector3 currentMousePos = UtilsClass.GetMouseWorldPosition();
+
+            Vector3 lowerLeft = new Vector3(
+                Mathf.Min(startPos.x, currentMousePos.x),
+                Mathf.Min(startPos.y, currentMousePos.y)
+            );
+
+            Vector3 upperRight = new Vector3(
+                Mathf.Max(startPos.x, currentMousePos.x),
+                Mathf.Max(startPos.y, currentMousePos.y)
+            );
+
+            selectionAreaTransform.position = lowerLeft;
+
+            selectionAreaTransform.localScale = upperRight - lowerLeft;
+        }
+
+        private void HandleUnitMovement()
+        {
+            Vector3 targetPosition = UtilsClass.GetMouseWorldPosition();
+
+            foreach (var unit in selectedSquadUnits)
+            {
+                Vector3 randomPos = targetPosition + new Vector3(Random.insideUnitCircle.x, Random.insideUnitCircle.y, 0f) * circleRadius;
+
+                unit.SetTargetPosition(randomPos);
+            }
+        }
+
+        private void DeselectUnits()
+        {
+            foreach (var unit in selectedSquadUnits)
+            {
+                unit.SetSelectedVisible(false);
+            }
+
+            selectedSquadUnits.Clear();
         }
     }
 }
